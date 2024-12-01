ecc511caad35a079482eb625fad7dffb457a57e7
Andrej Vencelj <andrej.vencelj.007@gmail.com>
2024-12-01 17:35:54 +0100

    day1 done!

--- days/day1.py
+++ days/day1.py
@@ -1,3 +1,36 @@
-def part1():
-    return "part1!!!"
+from util import read_file
 
+def part1():
+    input_list = read_file()
+    left_list = []
+    right_list = []
+    total_distance = 0
+
+    for line in input_list:
+        nums = [int(num) for num in line.split()]
+        left_list.append(nums[0])
+        right_list.append(nums[1])
+
+    left_list.sort()
+    right_list.sort()
+
+    for left, right in zip(left_list, right_list):
+        total_distance += abs(left - right)
+
+    return total_distance
+
+def part2():
+    input_list = read_file()
+    left_list = []
+    right_list = []
+    similarity_score = 0
+
+    for line in input_list:
+        nums = [int(num) for num in line.split()]
+        left_list.append(nums[0])
+        right_list.append(nums[1])
+
+    for element in left_list:
+        similarity_score += right_list.count(element) * element
+
+    return similarity_score
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import importlib
 
-day = 0
-part = 1
+day = None
+part = None
 
 def get_user_input():
     try:
         day = int(input("day: "))
-        if day not in [1, 25]:
+        if day not in range(1, 25):
             raise ValueError("Day must be between 1 and 25.")
         part = int(input("part: "))
         if part not in [1, 2]:
@@ -15,7 +15,6 @@ def get_user_input():
     except ValueError as e:
         print(f"Invalid input: {e}")
         return None, None
-
 
 def execute_day_method(day, part):
     try:
@@ -36,9 +35,7 @@ def execute_day_method(day, part):
     except Exception as e:
         print(f"An error occurred: {e}")
 
-
 if __name__ == "__main__":
-    if day == 0:
+    if day is None or part is None:
         day, part = get_user_input()
-    if day is not None and part is not None:
-        execute_day_method(day, part)
+    execute_day_method(day, part)
